c229008d56ece9fe96b7d24525e35e74167c2dc4
Malhar <awesoedu@gmail.com>
2017-06-29 12:51:55 +0000

    yo dawg it up

--- That_was_easy/main.js
+++ That_was_easy/main.js
@@ -2,4 +2,10 @@ function sayThatWasEasy() {
 var thatWasEasy = new Audio("that_was_easy.mp3");
 thatWasEasy.play();
 }
+$(document).keypress(delegateKeyPress)
 $("#easy").on("click", sayThatWasEasy);
+function delegateKeyPress(event){
+if(event.charCode == 32){
+$("#easy").trigger("click")
+}
+}
